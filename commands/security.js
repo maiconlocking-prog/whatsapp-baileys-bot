a64d308ca3a59266d8a74b02bd32cc89b1b83a7f
maiconlocking-prog <maiconlocking@gmail.com>
2026-06-02 16:33:45 -0300

    Add security commands module

---
+++ commands/security.js
@@ -0,0 +1,11 @@
+// Security commands
+const security = {
+  async ban(sock, msg) {
+    // Ban logic
+  },
+  async antilink() {
+    // Anti link implementation
+  }
+};
+
+module.exports = security;
